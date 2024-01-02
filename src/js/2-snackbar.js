53b831ca2a933b1faa435a275406523a45cf319f
Denysit <141247457+Denysit@users.noreply.github.com>
2024-01-03 00:15:24 +0200

    Update 2-snackbar.js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,6 +2,8 @@
 
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+// import iconSucsses from "../img/bi_check2-circle.svg";
+// import iconError from "../img/bi_x-octagon.svg";
 
 
 const section = document.querySelector('section');
@@ -67,7 +69,7 @@ form.addEventListener('submit', event => {
         messageColor: '#FFF',
         position: 'topRight',
         backgroundColor: '#59A10D',
-        iconUrl: '../img/bi_check2-circle.svg',
+        iconUrl: 'img/bi_check2-circle.svg',
 
 });
     })
@@ -80,7 +82,7 @@ form.addEventListener('submit', event => {
         messageColor: '#FFF',
         position: 'topRight',
         backgroundColor: '#EF4040',
-        iconUrl: '../img/bi_x-octagon.svg',
+        iconUrl: 'img/bi_x-octagon.svg',
 
 });
       
