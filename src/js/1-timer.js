e634c217dc25e8128361e535c4f73838e924ca98
Denysit <141247457+Denysit@users.noreply.github.com>
2024-01-02 23:47:39 +0200

    update

---
+++ src/js/1-timer.js
@@ -0,0 +1,154 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css"
+
+
+
+const body = document.querySelector(".container-timer");
+body.innerHTML = `
+    <input type="text" id="datetime-picker" />
+    <button type="button" data-start>Start</button>
+    <div class="timer">
+      <div class="field">
+        <span class="value" data-days>00</span>
+        <span class="label">Days</span>
+      </div>
+      <div class="field">
+        <span class="value" data-hours>00</span>
+        <span class="label">Hours</span>
+      </div>
+      <div class="field">
+        <span class="value" data-minutes>00</span>
+        <span class="label">Minutes</span>
+      </div>
+      <div class="field">
+        <span class="value" data-seconds>00</span>
+        <span class="label">Seconds</span>
+      </div>
+    </div>`;
+
+
+const input = document.querySelector("#datetime-picker");
+const startButton = document.querySelector("[data-start]");
+let userSelectedDate = null;
+let timer; 
+startButton.disabled = true;
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+
+    if (!timer || !timer.isActive()) {
+      const currentDate = new Date();
+      if (userSelectedDate < currentDate) {
+    iziToast.error({
+     title: 'Error',
+      message: 'Please choose a date in the future',
+      position: 'topRight',
+      titleColor: '#FFF',
+      messageColor: '#FFF',
+      backgroundColor: '#EF4040',
+      closeOnEscape: true,
+      iconUrl: './img/bi_x-octagon.svg',
+        });
+        startButton.disabled = true;
+      } else {
+        startButton.disabled = false;
+      }
+    }
+
+    console.log(userSelectedDate);
+  },
+};
+
+flatpickr("#datetime-picker", options);
+
+class Timer {
+  timerElement = null;
+  intervalId = null;
+
+  constructor(endDate, onUpdate) {
+    this.endDate = endDate;
+    this.onUpdate = onUpdate;
+  }
+
+  isActive() {
+    return this.intervalId !== null;
+  }
+
+  start() {
+    const updateInterval = 1000;
+
+    const updateTimer = () => {
+      const now = new Date();
+      const timeDifference = this.endDate - now;
+
+      if (timeDifference <= 0) {
+        this.stop();
+        this.onUpdate(convertMs(0));
+        startButton.disabled = false; 
+      } else {
+        const timeRemaining = convertMs(timeDifference);
+        this.onUpdate(timeRemaining);
+        setTimeout(updateTimer, updateInterval);
+      }
+    };
+
+    if (!this.isActive()) {
+      startButton.disabled = true;
+      this.intervalId = setInterval(updateTimer, updateInterval);
+    }
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateTimerDisplay({ days, hours, minutes, seconds }) {
+  const daysElement = document.querySelector('[data-days]');
+  const hoursElement = document.querySelector('[data-hours]');
+  const minutesElement = document.querySelector('[data-minutes]');
+  const secondsElement = document.querySelector('[data-seconds]');
+
+  daysElement.textContent = addLeadingZero(days);
+  hoursElement.textContent = addLeadingZero(hours);
+  minutesElement.textContent = addLeadingZero(minutes);
+  secondsElement.textContent = addLeadingZero(seconds);
+}
+
+startButton.addEventListener('click', () => {
+  timer = new Timer(userSelectedDate, updateTimerDisplay);
+  timer.start();
+  startButton.disabled = true;
+});
+
+
+
+
+
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,91 @@
+
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const section = document.querySelector('section');
+
+section.insertAdjacentHTML ('afterend', `<form class="form">
+  <label>
+    Delay (ms)
+    <input type="number" name="delay" required />
+  </label>
+
+  <fieldset>
+    <legend>State</legend>
+    <label>
+      <input type="radio" name="state" value="fulfilled" required />
+      Fulfilled
+    </label>
+    <label>
+      <input type="radio" name="state" value="rejected" required />
+      Rejected
+    </label>
+  </fieldset>
+
+  <button type="submit">Create notification</button>
+</form>`);
+
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+
+  event.preventDefault();
+  
+  const delayInput = document.querySelector('[name="delay"]')
+  const stateInput = document.querySelector('[name="state"]:checked');
+  
+  if (!delayInput || !stateInput) {
+    return;
+  }
+
+  const delay = parseInt(delayInput.value);
+  
+  const promise = new Promise((resolve, reject) => {
+      
+    setTimeout(() => {
+      
+      const state = stateInput.value;
+
+     if (state === 'rejected') {
+      reject('');
+     }
+     else {
+      resolve('');
+    }
+    }, delay);
+        
+  });
+
+  promise
+    .then(success => {
+      iziToast.show({
+        title: 'OK',
+        titleColor: '#FFF',
+        message: `Fulfilled promise in ${delay}ms`,
+        messageColor: '#FFF',
+        position: 'topRight',
+        backgroundColor: '#59A10D',
+        iconUrl: './img/bi_check2-circle.svg',
+
+});
+    })
+
+    .catch(error => {
+      iziToast.show({
+        message: `Rejected promise in ${delay}ms`,
+        title: 'Error',
+        titleColor: '#FFF',
+        messageColor: '#FFF',
+        position: 'topRight',
+        backgroundColor: '#EF4040',
+        iconUrl: './img/bi_x-octagon.svg',
+
+});
+      
+})
+
+});
+
+
